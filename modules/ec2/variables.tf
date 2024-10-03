0003999bbe0dde8ea39aea891ee6d315cbcc5a7b
Shashwat Chaturvedi <71917863+ShashwatChaturvedi@users.noreply.github.com>
2024-10-03 12:00:33 +0530

    Update variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,19 +1,13 @@
-variable "ami_id" {
-  description = "The AMI ID to use for the EC2 instance"
-  default     = "ami-0522ab6e1ddcc7055"  # Replace with your AMI ID
+variable "cluster_name" {
+  description = "The name of the EKS cluster"
+  default     = "my-eks-cluster"
 }
 
-variable "instance_type" {
-  description = "The instance type to use for the EC2 instance"
-  default     = "t2.micro"
+variable "vpc_id" {
+  description = "The VPC ID where EKS will be deployed"
 }
 
-variable "subnet_id" {
-  description = "The ID of the subnet where the EC2 instance will be launched"
-  type        = string
-}
-
-variable "security_group_id" {
-  description = "The ID of the security group to attach to the EC2 instance"
-  type        = string
+variable "public_subnet_ids" {
+  description = "Public Subnet IDs for EKS"
+  type        = list(string)
 }
