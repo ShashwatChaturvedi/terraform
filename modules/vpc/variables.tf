ca5528f0b77e1b4165f7d677f51af39f7a433fe1
Shashwat Chaturvedi <71917863+ShashwatChaturvedi@users.noreply.github.com>
2024-10-03 12:05:20 +0530

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,4 +1,25 @@
-variable "vpc_cidr_block" {
-  description = "The CIDR block for the VPC"
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
   default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+variable "vpc_name" {
+  description = "The name of the VPC"
+  type        = string
+  default     = "my-vpc"
 }
