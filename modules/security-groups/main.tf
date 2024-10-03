337c7926420d4bf4fb59b4ab9a819e9bdef01129
Shashwat Chaturvedi <71917863+ShashwatChaturvedi@users.noreply.github.com>
2024-10-03 12:03:49 +0530

    Update main.tf

--- modules/security-groups/main.tf
+++ modules/security-groups/main.tf
@@ -1,11 +1,16 @@
-resource "aws_security_group" "main" {
-  name        = "allow-all-traffic"
-  description = "Security group that allows all traffic"
-  vpc_id      = var.vpc_id
+resource "aws_security_group" "eks_node_group" {
+  vpc_id = var.vpc_id
 
   ingress {
-    from_port   = 80               # Example for HTTP
-    to_port     = 80
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
