a18470aa9c3207b8ae2c77ac0a88f65a927939f0
Shashwat Chaturvedi <71917863+ShashwatChaturvedi@users.noreply.github.com>
2024-10-03 12:05:55 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,19 @@ output "vpc_id" {
 output "eks_cluster_name" {
   value = module.eks.cluster_name
 }
+
+output "eks_node_group" {
+  value = aws_eks_node_group.eks_node_group.id
+}
+
+output "ec2_instance_id" {
+  value = module.ec2.instance_id
+}
+
+output "rds_instance_endpoint" {
+  value = module.rds.db_instance_endpoint
+}
+
+output "security_group_id" {
+  value = module.security_groups.sg_id
+}
