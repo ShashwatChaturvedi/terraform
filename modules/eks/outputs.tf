47cb98424c3184ec008aa76756008ca26933cc47
Shashwat Chaturvedi <71917863+ShashwatChaturvedi@users.noreply.github.com>
2024-10-03 12:01:19 +0530

    Update outputs.tf

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -1,4 +1,15 @@
 output "cluster_name" {
-  description = "The name of the EKS cluster"
-  value       = module.eks.cluster_name
+  value = aws_eks_cluster.this.name
+}
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.this.endpoint
+}
+
+output "cluster_security_group_id" {
+  value = aws_eks_cluster.this.vpc_config[0].cluster_security_group_id
+}
+
+output "worker_iam_role_arn" {
+  value = aws_iam_role.eks_role.arn
 }
