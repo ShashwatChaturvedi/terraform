216cf1fde1ddc1c2a1c9c14dabe1b6c95cd88436
Shashwat Chaturvedi <71917863+ShashwatChaturvedi@users.noreply.github.com>
2024-10-03 12:02:36 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,25 +1,10 @@
-resource "aws_eks_cluster" "this" {
-  name     = var.cluster_name
-  role_arn = aws_iam_role.eks_role.arn
+resource "aws_instance" "this" {
+  ami             = var.ami
+  instance_type   = var.instance_type
+  key_name        = var.key_name
+  subnet_id       = var.subnet_id
 
-  vpc_config {
-    subnet_ids = var.public_subnet_ids
+  tags = {
+    Name = var.instance_name
   }
-}
-
-resource "aws_iam_role" "eks_role" {
-  name = "${var.cluster_name}-eks-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
 }
